ba449e366c35efd5b0127766c46415e81c81dbd5
Jeremy <jeremy.d.vu@gmail.com>
2026-03-15 20:20:36 -0400

    Treat THK Revisit as gate-restore state

--- Source/SaveStates/RoomSpecific.cs
+++ Source/SaveStates/RoomSpecific.cs
@@ -28,6 +28,7 @@ namespace DebugMod
 
         private static readonly HashSet<string> ThkEngagedBattleStates = new HashSet<string>(StringComparer.Ordinal)
         {
+            "Revisit",
             "Free Pause",
             "Struggle",
             "Break Antic",
@@ -270,7 +271,7 @@ namespace DebugMod
 
         private static bool IsThkScene(string? sceneName)
         {
-            return string.Equals(sceneName, ThkSceneName, StringComparison.Ordinal);
+            return string.Equals(sceneName, ThkSceneName, StringComparison.OrdinalIgnoreCase);
         }
 
         private static bool ShouldForceThkGateCloseOnLoad(out string reason)
